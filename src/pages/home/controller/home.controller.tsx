a2d2989019331f620ed48e6d25e7f48a63460f15
Luan <luanhsantos14@gmail.com>
2025-09-03 17:38:39 -0300

    feat: adicionando catálogo ao site

--- src/pages/home/controller/home.controller.tsx
+++ src/pages/home/controller/home.controller.tsx
@@ -1,7 +1,37 @@
 import Home from "../view/home.view";
+import { useEffect, useState } from "react";
+import type { Product } from "../../products/types";
+import { getProducts } from "../../../services/product-service";
 
 export default function HomeController() {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const productList = await getProducts();
+                setProducts(productList);
+            } catch (error) {
+                console.error("Erro ao buscar produtos:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchProducts();
+    }, []);
+
+    const handleAddToCart = (product: Product) => {
+        // Implementar
+        console.log("Produto adicionado ao carrinho:", product.name);
+    }
+
     return (
-        <Home />
+        <Home 
+            isLoading={isLoading}
+            products={products}
+            handleAddToCart={handleAddToCart}
+        />
     )
 }
--- src/pages/home/view/home.view.tsx
+++ src/pages/home/view/home.view.tsx
@@ -1,8 +1,28 @@
-export default function Home() {
+import styles from '../styles/home.module.css';
+import { ProductCard } from "../../products/components/product-card";
+import type { HomeProps } from '../types';
+
+export default function Home({
+    isLoading,
+    products,
+    handleAddToCart
+}: Readonly<HomeProps>) {
+    if (isLoading) {
+        return <p>A carregar produtos...</p>;
+    } 
+
     return (
         <div>
-            <h1>Home Page</h1>
-            <p>Catalog</p>
+            <h1>Nossos Produtos</h1>
+            <div className={styles.productGrid}>
+                {products.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        product={product}
+                        onAddToCart={handleAddToCart}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
