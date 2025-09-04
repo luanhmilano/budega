798c0fb980ca5e9d10f033b18a77e8c2bfe074d9
Luan <luanhsantos14@gmail.com>
2025-09-04 12:56:26 -0300

    feat(products): adicionando controller e melhorando mocks

--- src/pages/products/controller/products.controller.tsx
+++ src/pages/products/controller/products.controller.tsx
@@ -1,5 +1,68 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Products from "../view/products.view";
+import type { Product } from '../types';
+import { getProductById } from '../../../services/product-service';
+import { formattedPrice } from '../../../utils/formatted-price';
 
 export default function ProductsController() {
-    return <Products />
+    const { id } = useParams<{ id: string }>();
+
+    const [product, setProduct] = useState<Product | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+    const [selectedVariants, setSelectedVariants] = useState<{ [key: string]: string }>({});
+
+    useEffect(() => {
+        if (!id) return;
+
+        const fetchProduct = async () => {
+            try {
+                const foundProduct = await getProductById(Number(id));
+                if (foundProduct) {
+                    setProduct(foundProduct);
+                    const initialVariants: { [key: string]: string} = {};
+                    foundProduct.variants?.forEach((variant) => {
+                        initialVariants[variant.name] = variant.options[0].value;
+                    });
+                    setSelectedVariants(initialVariants);
+                } else {
+                    setError("Produto não encontrado.");
+                    // setTimeout(() => navigate('/404'), 3000);
+                }
+            } catch (error) {
+                setError('Ocorreu um erro ao carregar o produto.')
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchProduct();
+    }, [id]);
+
+    const handleVariantChange = (variantName: string, value: string) => {
+        setSelectedVariants(prev => ({
+            ...prev,
+            [variantName]: value,
+        }));
+    };
+
+    const handleAddToCart = () => {
+        console.log('Adicionando ao carrinho (a implementar):', {
+            product,
+            selectedVariants,
+        });
+    };
+
+    return ( 
+        <Products 
+            isLoading={isLoading}
+            error={error}
+            product={product}
+            productPriceFormatted={product ? formattedPrice(product) : 'R$ 0,00'}
+            selectedVariants={selectedVariants}
+            handleVariantChange={handleVariantChange}
+            handleAddToCart={handleAddToCart}
+        />
+    )
 }
--- src/pages/products/data/produtcs-mock.ts
+++ src/pages/products/data/produtcs-mock.ts
@@ -2,45 +2,93 @@ import type { Product } from "../types";
 
 export const mockProducts: Product[] = [
     {
-        "id": 1,
-        "name": "Camisa",
-        "description": "Camisa de algodão",
-        "price": 49.99,
-        "image": "https://placehold.co/600x400/orange/white"
+        id: 1,
+        name: "Camisa",
+        description: "Camisa de algodão",
+        price: 49.99,
+        image: "https://placehold.co/600x400/orange/white",
+        variants: [
+            {
+                name: "Tamanho",
+                options: [
+                    { label: "P", value: "p" },
+                    { label: "M", value: "m" },
+                    { label: "G", value: "g" }
+                ]
+            },
+            {
+                name: "Cor",
+                options: [
+                    { label: "Vermelho", value: "red" },
+                    { label: "Azul", value: "blue" },
+                    { label: "Verde", value: "green" }
+                ]
+            }
+        ]
     },
     {
-        "id": 2,
-        "name": "Calça",
-        "description": "Calça jeans",
-        "price": 89.99,
-        "image": "https://placehold.co/600x400/black/white"
+        id: 2,
+        name: "Calça",
+        description: "Calça jeans",
+        price: 89.99,
+        image: "https://placehold.co/600x400/black/white",
+        variants: [
+            {
+                name: "Tamanho",
+                options: [
+                    { label: "P", value: "p" },
+                    { label: "M", value: "m" },
+                    { label: "G", value: "g" }
+                ]
+            },
+        ]
     },
     {
-        "id": 3,
-        "name": "Tênis",
-        "description": "Tênis esportivo",
-        "price": 129.99,
-        "image": "https://placehold.co/600x400/blue/white"
+        id: 3,
+        name: "Tênis",
+        description: "Tênis esportivo",
+        price: 129.99,
+        image: "https://placehold.co/600x400/blue/white",
+        variants: [
+            {
+                name: "Tamanho",
+                options: [
+                    { label: "40", value: "p" },
+                    { label: "41", value: "m" },
+                    { label: "42", value: "g" }
+                ]
+            },
+        ]
     },
     {
-        "id": 4,
-        "name": "Jaqueta",
-        "description": "Jaqueta de couro",
-        "price": 199.99,
-        "image": "https://placehold.co/600x400/yellow/white"
+        id: 4,
+        name: "Jaqueta",
+        description: "Jaqueta de couro",
+        price: 199.99,
+        image: "https://placehold.co/600x400/yellow/white"
     },
     {
-        "id": 5,
-        "name": "Boné",
-        "description": "Boné de aba reta",
-        "price": 29.99,
-        "image": "https://placehold.co/600x400/red/white"
+        id: 5,
+        name: "Boné",
+        description: "Boné de aba reta",
+        price: 29.99,
+        image: "https://placehold.co/600x400/red/white",
+        variants: [
+            {
+                name: "Cor",
+                options: [
+                    { label: "Preto", value: "black" },
+                    { label: "Branco", value: "white" },
+                    { label: "Rosa", value: "pink" }
+                ]
+            }
+        ]
     },
     {
-        "id": 6,
-        "name": "Relógio",
-        "description": "Relógio de pulso",
-        "price": 399.99,
-        "image": "https://placehold.co/600x400/brown/black"
+        id: 6,
+        name: "Relógio",
+        description: "Relógio de pulso",
+        price: 399.99,
+        image: "https://placehold.co/600x400/brown/black"
     }
 ]
